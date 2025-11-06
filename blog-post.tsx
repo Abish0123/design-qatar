693bab44f1aa9d5af5d25eca4c44d4b9ef3e890c
Abish0123 <abishsuresh01@gmail.com>
2025-11-06 18:01:18 +0530

    Updated blog-post.tsx

--- blog-post.tsx
+++ blog-post.tsx
@@ -97,7 +97,7 @@ const blogPosts = [
     },
     {
       slug: "securing-construction-approvals-qatar",
-      image: "https://images.unsplash.com/photo-1589939705384-5185137a7f0f?w=1600&auto=format&fit=crop&q=60",
+      image: "https://media.istockphoto.com/id/155035293/photo/building-permit.jpg?s=612x612&w=0&k=20&c=UWwC-Im7dj_GgazxjElGw2bv2a_mrbnXMW0a6DiyryE=",
       category: "Approvals",
       author: "Taj Design Experts",
       date: "November 19, 2024",
